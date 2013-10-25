6520d661e2eadaf372f56345b91b2e524d463618
magomimmo <mimmo.cosenza@gmail.com>
2013-10-25 12:11:50 +0200

    added description, rollbacked the CLJS version and updated cljsbuild to 0.3.4

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
 (defproject hello-enfocus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The simplest enfocus sample"
+  :url "https://github.com/magomimmo/hello-enfocus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.2.0"
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1913"]
+                 [org.clojure/clojurescript "0.0-1847"]
                  [org.clojars.magomimmo/enfocus "2.0.1-SNAPSHOT"]]
-  :plugins [[lein-cljsbuild "0.3.3"]]
+  :plugins [[lein-cljsbuild "0.3.4"]]
+  :hooks [leiningen.cljsbuild]
 
   :cljsbuild {:builds {:whitespace
                        {:source-paths ["src/cljs"]
